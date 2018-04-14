c99b2fd76378b081cb74be03b89b3c987ec0d237
alnyan <qShadowp@gmail.com>
2018-04-14 14:54:21 +0300

    Added GDT loading for 32-bit bootstrap

--- src/loader/boot/multiboot.s
+++ src/loader/boot/multiboot.s
@@ -17,4 +17,15 @@
 .global multiboot_entry
 .align 16
 multiboot_entry:
+    lgdt (gdtr32)
+
     jmp loader_main
+
+.align 16
+gdt32:
+    .quad 0x0000000000000000
+    .quad 0x00CF9A000000FFFF
+    .quad 0x00CF92000000FFFF
+gdtr32:
+    .long gdt32
+    .word (gdtr32 - gdt32 - 1)
